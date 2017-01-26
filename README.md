2f54fa518d39fb9935a6749b8c759617a499cbd8
Rogier Slag <rogier.slag@gmail.com>
2017-01-26 15:09:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Redis-info-json
+
+A very simple script with Docker container to parse the output of `redis-cli` to readable json which can then be continued to be processed by another script.
+
+At [Magnet.me](https://magnet.me) we use it to monitor our redis cluster and feed the JSON data into other monitoring systems.
+
+You can run it like `redis-cli info | docker run -i --rm magnetme/redis-info-json`
---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+process.stdin.setEncoding('utf8');
+
+// This saves the redis data to a variable
+var data = "";
+var EMPTY_LINE = '';
+
+function isNumeric(num){
+	return !isNaN(num)
+}
+
+function isTrue(value) {
+	return value && (value === true || value === 'true');
+}
+
+function startProcessing(data) {
+	const lines = data.split("\n").filter((line) => line !== EMPTY_LINE);
+
+	const res = {};
+	let currentLevel;
+	lines.forEach((line) => {
+		if (line.startsWith('#')) {
+			currentLevel = line.substring(1).trim().toLowerCase();
+			res[currentLevel] = {};
+			return;
+		}
+		const splitted = line.split(':');
+		const key = splitted[0];
+		const value = isNumeric(splitted[1]) ? parseFloat(splitted[1]) : splitted[1];
+
+		if(currentLevel) {
+			res[currentLevel][key] = value;
+		} else {
+			res[key] = value;
+		}
+	});
+	writeOutputAsJson(res);
+}
+
+function writeOutputAsJson(data) {
+	console.log(JSON.stringify(data, null, isTrue(process.env.PRETTY_PRINT) ? 4 : 0));
+}
+
+process.stdin.on('readable', () => {
+	var chunk = process.stdin.read();
+	if (chunk !== null) {
+		data += chunk
+	}
+});
+
+process.stdin.on('end', () => {
+	startProcessing(data);
+});
